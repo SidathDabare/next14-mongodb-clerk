70e038cdd4ea4a18e7daaca128e99743ef0f2ca6
Sidath Dabare <sidath2007@gmail.com>
2024-10-23 23:25:37 +0200

    fix

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -22,7 +22,7 @@ export const connect = async () => {
   cached.promise =
     cached.promise ||
     mongoose.connect(MONGODB_URL, {
-      dbName: "clerk-next14-db",
+      dbName: "channaka-e-commerce",
       bufferCommands: false,
       connectTimeoutMS: 30000,
     });
